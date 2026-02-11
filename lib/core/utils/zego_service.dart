bca7da7ee8c0c1a02d6ba7cee581bbe9ec03523d
rahmamahmoud2 <rahmamahmoud.work2@gmail.com>
2026-02-11 22:12:09 +0200

    //

--- lib/core/utils/zego_service.dart
+++ lib/core/utils/zego_service.dart
@@ -1,77 +1,77 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:zego_uikit/zego_uikit.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
-import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:zego_uikit/zego_uikit.dart';
+// import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+// import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
 
-class ZegoService {
-  static final ZegoService _instance = ZegoService._();
-  factory ZegoService() => _instance;
-  ZegoService._();
+// class ZegoService {
+//   static final ZegoService _instance = ZegoService._();
+//   factory ZegoService() => _instance;
+//   ZegoService._();
 
-  final appID = int.parse(dotenv.env['ZEGO_APP_ID']!);
-  final appSign = dotenv.env['ZEGO_APP_SIGN']!;
+//   final appID = int.parse(dotenv.env['ZEGO_APP_ID']!);
+//   final appSign = dotenv.env['ZEGO_APP_SIGN']!;
 
-  void init({
-    required String userId,
-    required String userName,
-    required String fcmToken,
-  }) {
-    print(
-      '🔔 ZegoService Init - UserID: $userId, UserName: $userName, FCM: $fcmToken',
-    );
+//   void init({
+//     required String userId,
+//     required String userName,
+//     required String fcmToken,
+//   }) {
+//     print(
+//       '🔔 ZegoService Init - UserID: $userId, UserName: $userName, FCM: $fcmToken',
+//     );
 
-    final service = ZegoUIKitPrebuiltCallInvitationService();
+//     final service = ZegoUIKitPrebuiltCallInvitationService();
 
-    service.init(
-      notificationConfig: ZegoCallInvitationNotificationConfig(
-        androidNotificationConfig: ZegoCallAndroidNotificationConfig(
-          callIDVisibility: true,
-        ),
-        iOSNotificationConfig: ZegoCallIOSNotificationConfig(
-          isSandboxEnvironment: true,
-        ),
-      ),
-      ringtoneConfig: ZegoCallRingtoneConfig(
-        // Temporarily disable ringtone to test audio session issue
-        // incomingCallPath: "assets/sounds/ringTone.mp3",
-      ),
-      requireConfig: (ZegoCallInvitationData data) {
-        print('📞 Incoming call data: ${data.toString()}');
-        try {
-          var config = (data.invitees.length > 1)
-              ? ZegoCallInvitationType.videoCall == data.type
-                    ? ZegoUIKitPrebuiltCallConfig.groupVideoCall()
-                    : ZegoUIKitPrebuiltCallConfig.groupVoiceCall()
-              : ZegoCallInvitationType.videoCall == data.type
-              ? ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
-              : ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall();
-          // Modify your custom configurations here.
-          config.layout = ZegoLayout.gallery(
-            addBorderRadiusAndSpacingBetweenView: false,
-          );
-          return config;
-        } catch (e) {
-          print('❌ Error in requireConfig: $e');
-          // Return a default config if there's an error
-          return ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall();
-        }
-      },
-      appID: appID,
-      appSign: appSign,
-      userID: userId,
-      userName: userName,
-      plugins: [ZegoUIKitSignalingPlugin()],
-    );
+//     service.init(
+//       notificationConfig: ZegoCallInvitationNotificationConfig(
+//         androidNotificationConfig: ZegoCallAndroidNotificationConfig(
+//           callIDVisibility: true,
+//         ),
+//         iOSNotificationConfig: ZegoCallIOSNotificationConfig(
+//           isSandboxEnvironment: true,
+//         ),
+//       ),
+//       ringtoneConfig: ZegoCallRingtoneConfig(
+//         // Temporarily disable ringtone to test audio session issue
+//         // incomingCallPath: "assets/sounds/ringTone.mp3",
+//       ),
+//       requireConfig: (ZegoCallInvitationData data) {
+//         print('📞 Incoming call data: ${data.toString()}');
+//         try {
+//           var config = (data.invitees.length > 1)
+//               ? ZegoCallInvitationType.videoCall == data.type
+//                     ? ZegoUIKitPrebuiltCallConfig.groupVideoCall()
+//                     : ZegoUIKitPrebuiltCallConfig.groupVoiceCall()
+//               : ZegoCallInvitationType.videoCall == data.type
+//               ? ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall()
+//               : ZegoUIKitPrebuiltCallConfig.oneOnOneVoiceCall();
+//           // Modify your custom configurations here.
+//           config.layout = ZegoLayout.gallery(
+//             addBorderRadiusAndSpacingBetweenView: false,
+//           );
+//           return config;
+//         } catch (e) {
+//           print('❌ Error in requireConfig: $e');
+//           // Return a default config if there's an error
+//           return ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall();
+//         }
+//       },
+//       appID: appID,
+//       appSign: appSign,
+//       userID: userId,
+//       userName: userName,
+//       plugins: [ZegoUIKitSignalingPlugin()],
+//     );
 
-    /// ⭐ أهم سطر للمكالمات المغلقة
-    // The service is already initialized with plugins, which should handle push notifications
-    // Make sure FCM token is properly configured in your Zego dashboard
-    ZegoUIKit().init(appID: appID, appSign: appSign);
+//     /// ⭐ أهم سطر للمكالمات المغلقة
+//     // The service is already initialized with plugins, which should handle push notifications
+//     // Make sure FCM token is properly configured in your Zego dashboard
+//     ZegoUIKit().init(appID: appID, appSign: appSign);
 
-    print('✅ ZegoService initialized successfully');
-  }
+//     print('✅ ZegoService initialized successfully');
+//   }
 
-  void unInit() {
-    ZegoUIKitPrebuiltCallInvitationService().uninit();
-  }
-}
+//   void unInit() {
+//     ZegoUIKitPrebuiltCallInvitationService().uninit();
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'core/app_services/remote_services/service_locator.dart';
 import 'core/utils/bloc_observer.dart';
 import 'core/utils/zego_service.dart';
@@ -12,9 +12,9 @@ import 'core/utils/zego_service.dart';
 import 'lang/codegen_loader.g.dart';
 import 'main_imports.dart';
 import 'my_app.dart';
-import 'package:zego_uikit/zego_uikit.dart';
-import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
-import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+// import 'package:zego_uikit/zego_uikit.dart';
+// import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+// import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
 
 Future<void> _requestPermissions() async {
   await [
@@ -33,7 +33,7 @@ void main() async {
   // TODO: Re-enable Firebase Call Service after fixing Gradle
   // await FirebaseCallService().initialize();
 
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   if (Platform.isIOS) {
     await Firebase.initializeApp();
   } else if (Platform.isAndroid) {
@@ -76,69 +76,70 @@ void main() async {
   Bloc.observer = MyBlocObserver();
 
   /// zego
-  ZegoUIKitPrebuiltCallInvitationService().setNavigatorKey(navigateKey);
+  // ZegoUIKitPrebuiltCallInvitationService().setNavigatorKey(navigateKey);
 
-  await ZegoUIKit().initLog().then((value) async {
-    await ZegoUIKitPrebuiltCallInvitationService().useSystemCallingUI([
-      ZegoUIKitSignalingPlugin(),
-    ]);
-    runApp(
-      EasyLocalization(
-        startLocale: const Locale('en', ""),
-        supportedLocales: const [Locale('ar', ""), Locale('en', "")],
-        path: 'lib/lang',
-        saveLocale: true,
-        fallbackLocale: const Locale('en', ""),
-        useOnlyLangCode: true,
-        assetLoader: const CodegenLoader(),
-        child: MyApp(navigatorKey: navigateKey),
-      ),
-    );
-  });
-  String userName = CacheHelper.getData(key: "userName").toString();
-  if (userName.isEmpty || userName == "null") {
-    userName = "Teacher";
-  }
-
-  ZegoService().init(
-    userId: CacheHelper.getData(key: "userId").toString(),
-    userName: userName,
-    fcmToken: CacheHelper.getData(key: "fcmToken").toString(),
+  // await ZegoUIKit().initLog().then((value) async {
+  //   await ZegoUIKitPrebuiltCallInvitationService().useSystemCallingUI([
+  //     ZegoUIKitSignalingPlugin(),
+  // ]);
+  runApp(
+    EasyLocalization(
+      startLocale: const Locale('en', ""),
+      supportedLocales: const [Locale('ar', ""), Locale('en', "")],
+      path: 'lib/lang',
+      saveLocale: true,
+      fallbackLocale: const Locale('en', ""),
+      useOnlyLangCode: true,
+      assetLoader: const CodegenLoader(),
+      child: MyApp(navigatorKey: navigateKey),
+    ),
   );
-  WidgetsBinding.instance.addPostFrameCallback((_) {
-    /// all time-consuming and waiting operations are DONE!!
-    /// jump to call page page if app active by offline call
-    ZegoUIKitPrebuiltCallInvitationService().enterAcceptedOfflineCall();
-  });
-}
+  // });
+  String userName = CacheHelper.getData(key: "userName").toString();
+  //   if (userName.isEmpty || userName == "null") {
+  //     userName = "Teacher";
+  //   }
 
-Future<void> getFcmToken() async {
-  try {
-    if (Platform.isIOS) {
-      final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
-      if (apnsToken == null) {
-        log('apnsToken is null');
-        return;
-      } else {
-        log("apnsToken = $apnsToken");
-        CacheHelper.saveData(key: "fcmToken", value: apnsToken);
+  //   ZegoService().init(
+  //     userId: CacheHelper.getData(key: "userId").toString(),
+  //     userName: userName,
+  //     fcmToken: CacheHelper.getData(key: "fcmToken").toString(),
+  //   );
+  //   WidgetsBinding.instance.addPostFrameCallback((_) {
+  //     /// all time-consuming and waiting operations are DONE!!
+  //     /// jump to call page page if app active by offline call
+  //     // ZegoUIKitPrebuiltCallInvitationService().enterAcceptedOfflineCall();
+  //   });
+  // }
+
+  Future<void> getFcmToken() async {
+    try {
+      if (Platform.isIOS) {
+        final apnsToken = await FirebaseMessaging.instance.getAPNSToken();
+        if (apnsToken == null) {
+          log('apnsToken is null');
+          return;
+        } else {
+          log("apnsToken = $apnsToken");
+          CacheHelper.saveData(key: "fcmToken", value: apnsToken);
+        }
       }
-    }
-    if (Platform.isAndroid) {
-      final fcmToken = await FirebaseMessaging.instance.getToken();
-      if (fcmToken == null) {
-        log('fcmToken is null');
-        return;
-      } else {
-        CacheHelper.saveData(key: "fcmToken", value: fcmToken);
+      if (Platform.isAndroid) {
+        final fcmToken = await FirebaseMessaging.instance.getToken();
+        if (fcmToken == null) {
+          log('fcmToken is null');
+          return;
+        } else {
+          CacheHelper.saveData(key: "fcmToken", value: fcmToken);
+        }
       }
+      FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {}).onError((
+        err,
+      ) {
+        log(err.toString());
+      });
+    } catch (e) {
+      log(e.toString());
     }
-    FirebaseMessaging.instance.onTokenRefresh.listen((fcmToken) {}).onError((
-      err,
-    ) {
-      log(err.toString());
-    });
-  } catch (e) {
-    log(e.toString());
   }
 }
